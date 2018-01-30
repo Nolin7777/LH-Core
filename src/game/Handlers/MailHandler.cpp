14a8b7645a2804d5310baa503e06d68be5ba49d3
Prithu Parker <prithu@iinet.net.au>
2018-01-30 17:47:50 +1100

    Don't consume money if unable to send mail due to being silenced, just silently fail

--- src/game/Handlers/MailHandler.cpp
+++ src/game/Handlers/MailHandler.cpp
@@ -294,9 +294,6 @@ void WorldSession::HandleSendMailCallback(WorldSession::AsyncMailSendRequest* re
     if (loadedPlayer->GetSession()->GetAnticheat()->IsSilenced())
     {
         pl->SendMailResult(0, MAIL_SEND, MAIL_OK);
-
-        loadedPlayer->ModifyMoney(-int32(reqmoney));
-
         return;
     }
 

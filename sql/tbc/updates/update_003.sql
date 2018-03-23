9d50aeeca79aada936eb209720100c5024e9b614
Kittnz <frederik156@hotmail.com>
2018-03-23 20:36:06 +0100

    Fix SQL

--- sql/tbc/updates/update_003.sql
+++ sql/tbc/updates/update_003.sql
@@ -1,3 +1,23 @@
+ALTER TABLE `npc_vendor`
+	ADD COLUMN `ExtendedCost` MEDIUMINT(8) NULL AFTER `incrtime`,
+	ADD COLUMN `condition_id` MEDIUMINT(8) NULL AFTER `ExtendedCost`;
+
+ALTER TABLE `npc_vendor`
+	ALTER `ExtendedCost` DROP DEFAULT,
+	ALTER `condition_id` DROP DEFAULT;
+ALTER TABLE `npc_vendor`
+	CHANGE COLUMN `ExtendedCost` `ExtendedCost` MEDIUMINT(8) UNSIGNED NOT NULL AFTER `incrtime`,
+	CHANGE COLUMN `condition_id` `condition_id` MEDIUMINT(8) UNSIGNED NOT NULL AFTER `ExtendedCost`;
+
+
 ALTER TABLE `npc_vendor_template`
-	CHANGE COLUMN `ExtendedCost` `ExtendedCost` MEDIUMINT(8) NULL DEFAULT NULL AFTER `incrtime`,
-	CHANGE COLUMN `condition_id` `condition_id` MEDIUMINT(8) NULL DEFAULT NULL AFTER `ExtendedCost`;
+	ADD COLUMN `ExtendedCost` MEDIUMINT(8) NULL AFTER `incrtime`,
+	ADD COLUMN `condition_id` MEDIUMINT(8) NULL AFTER `ExtendedCost`;
+
+ALTER TABLE `npc_vendor_template`
+	ALTER `ExtendedCost` DROP DEFAULT,
+	ALTER `condition_id` DROP DEFAULT;
+
+ALTER TABLE `npc_vendor_template`
+	CHANGE COLUMN `ExtendedCost` `ExtendedCost` MEDIUMINT(8) UNSIGNED NOT NULL AFTER `incrtime`,
+	CHANGE COLUMN `condition_id` `condition_id` MEDIUMINT(8) UNSIGNED NOT NULL AFTER `ExtendedCost`;

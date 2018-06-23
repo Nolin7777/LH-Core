ff1bdc2b005fe451d96252af3b122ca3ed640ac6
bladez <bladez.lightshope@gmail.com>
2018-06-23 17:49:20 +1000

    MC attune done

--- sql/custom/northdale_mc.sql
+++ sql/custom/northdale_mc.sql
@@ -1,5 +1,12 @@
 -- --------------------------------
 -- MC Teleport avail in 1.2 (no run through BRD)
+-- Also core does not support resing at BRD if you die in MC
 -- --------------------------------
 UPDATE `areatrigger_teleport` SET `patch` = 0 WHERE `id` IN (3528, 3529);
-UPDATE 
+
+UPDATE `quest_template` SET `patch` = 0 WHERE `entry` IN (7487, 7848);
+UPDATE `creature_questrelation` SET `patch` = 0 WHERE `quest` IN (7487, 7848);
+UPDATE `creature_involvedrelation` SET `patch` = 0 WHERE `quest` IN (7487, 7848);
+
+-- Core Fragment object
+UPDATE `gameobject` SET `patch_min` = 0 WHERE `id` = 179553;

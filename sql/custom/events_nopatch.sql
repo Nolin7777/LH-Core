db7c545c71e1f92d1955278402f9ef9b74f75339
bladez <bladez.lightshope@gmail.com>
2018-06-23 20:22:25 +1000

    Events patch WIP Fresh spawn rates

--- sql/custom/events_nopatch.sql
+++ sql/custom/events_nopatch.sql
@@ -1,1 +1,27 @@
 -- Remove patch requirement from all event quests, objects and creatures
+
+-- First remove patch requirements on quest reward and sources that are related to
+-- game events
+UPDATE item_template
+SET patch = 0
+WHERE entry IN (
+    SELECT RewItemId1 FROM quest_template qt JOIN game_event_quest ge on qt.entry = ge.quest
+    UNION
+    SELECT RewItemId2 FROM quest_template qt JOIN game_event_quest ge on qt.entry = ge.quest
+    UNION
+    SELECT RewItemId3 FROM quest_template qt JOIN game_event_quest ge on qt.entry = ge.quest
+    UNION
+    SELECT RewItemId4 FROM quest_template qt JOIN game_event_quest ge on qt.entry = ge.quest
+    UNION
+    SELECT ReqItemId1 FROM quest_template qt JOIN game_event_quest ge on qt.entry = ge.quest
+    UNION
+    SELECT ReqItemId2 FROM quest_template qt JOIN game_event_quest ge on qt.entry = ge.quest
+    UNION
+    SELECT ReqItemId3 FROM quest_template qt JOIN game_event_quest ge on qt.entry = ge.quest
+    UNION
+    SELECT ReqItemId4 FROM quest_template qt JOIN game_event_quest ge on qt.entry = ge.quest);
+
+
+UPDATE quest_template qt
+JOIN game_event_quest ge ON qt.entry = ge.quest
+SET qt.patch = 0, ge.patch = 0;
